29389200882730973ca4ae9cbae7083a7c17f2f9
Alex Vandiver <cpan@chmrr.net>
2014-08-05 22:31:43 +0000

    Import of ALEXMV/RT-Extension-UsernameFormat-Organization-1.00 from CPAN.
    gitpan-cpan-distribution: RT-Extension-UsernameFormat-Organization
    gitpan-cpan-version:      1.00
    gitpan-cpan-path:         ALEXMV/RT-Extension-UsernameFormat-Organization-1.00.tar.gz
    gitpan-cpan-author:       ALEXMV
    gitpan-cpan-maturity:     released

--- lib/RT/Extension/UsernameFormat/Organization.pm
+++ lib/RT/Extension/UsernameFormat/Organization.pm
@@ -2,72 +2,64 @@ use strict;
 use warnings;
 package RT::Extension::UsernameFormat::Organization;
 
-our $VERSION = '0.01';
+our $VERSION = '1.00';
 
 =head1 NAME
 
-RT-Extension-UsernameFormat-Organization - Adds a username format option for "Name (Org, City, Country)"
+RT-Extension-UsernameFormat-Organization - Adds a username format option for "Name [Org, City, Country]"
 
 =head1 DESCRIPTION
 
-Adds "Name (Organization, City, Country)" to the "Username format" preference.
+Adds "Name [Organization, City, Country]" to the "Username format" preference.
 
 Only non-empty values are shown, so if your users only have an organization and
-city you'd see something like "John Smith (Best Practical, Boston)" without any
+city you'd see something like "John Smith [Best Practical, Boston]" without any
 country.  The same goes for Organization and City.
-
-This extension makes City and Country a B<public> field, meaning any user can
-see any other user's value.  Default RT normally prevents non-administrators
-from seeing City and Country.
 
 =cut
 
 my $meta = RT->Config->Meta('UsernameFormat');
 push @{$meta->{WidgetArguments}{Values}}, 'organization';
-$meta->{WidgetArguments}{ValuesLabel}{organization} = 'Name (Organization, City, Country)';
+$meta->{WidgetArguments}{ValuesLabel}{organization} = 'Name [Organization, City, Country]';
 
 package RT::User;
 no warnings 'redefine';
 
-# Ensure these user fields are readable by all users
-sub RT::User::_VendorAccessible {
-    my %columns;
-    $columns{$_} = { public => 1 } for qw(
-        Organization
-        City
-        Country
-    );
-    return \%columns;
-}
+sub _FormatUserOrganization {
+    my $self = shift;
+    my %args = @_;
 
-# Required to work around a loading quirk
-__PACKAGE__->_BuildTableAttributes;
+    my $value = $self->_FormatUserRole(@_);
+
+    if ($args{User}) {
+        my $org = join ", ", grep { defined && length }
+            map { $args{User}->$_ }
+                qw(Organization City Country);
+        $value .= " [$org]" if $org;
+    }
+
+    return $value;
+}
 
 package RT::Extension::UsernameFormat::Organization;
 
-=head1 INSTALLATION 
+=head1 INSTALLATION
 
 =over
 
-=item perl Makefile.PL
+=item C<perl Makefile.PL>
 
-=item make
+=item C<make>
 
-=item make install
+=item C<make install>
 
 May need root permissions
 
-=item Edit your /opt/rt4/etc/RT_SiteConfig.pm
+=item Edit your F</opt/rt4/etc/RT_SiteConfig.pm>
 
 Add this line:
 
-    Set(@Plugins, qw(RT::Extension::UsernameFormat::Organization));
-
-or add C<RT::Extension::UsernameFormat::Organization> to your existing C<@Plugins> line.
-
-=item Clear your mason cache
-
-    rm -rf /opt/rt4/var/mason_data/obj
+    Plugin('RT::Extension::UsernameFormat::Organization');
 
 =item Restart your webserver
 
@@ -88,7 +80,7 @@ or L<bug-RT-Extension-UsernameFormat-Organization@rt.cpan.org>.
 
 =head1 LICENSE AND COPYRIGHT
 
-This software is Copyright (c) 2012 by Best Practical Solutions
+This software is Copyright (c) 2012-2014 by Best Practical Solutions
 
 This is free software, licensed under:
 
